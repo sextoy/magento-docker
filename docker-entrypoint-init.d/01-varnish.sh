f23f5082fed09900f4dc851671cc296fb78a1be7
Daniel Lashua <daniel@inklog.net>
2020-11-13 07:19:10 -0600

    add varnish config

---
+++ docker-entrypoint-init.d/01-varnish.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+magento config:set system/full_page_cache/caching_application 1
+magento config:set system/full_page_cache/varnish/backend_host magento
+magento config:set system/full_page_cache/varnish/backend_port 80
+magento config:set system/full_page_cache/varnish/grace_period 300
